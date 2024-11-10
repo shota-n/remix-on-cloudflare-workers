776c51faf21d58c54f7b11386154f3abbbe6786b
shota-n <naka35518@gmail.com>
2024-11-10 20:08:31 +0900

    add hono

--- server.ts
+++ server.ts
@@ -1,37 +1,46 @@
 import { type ServerBuild, createRequestHandler } from "@remix-run/cloudflare";
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+import { Hono } from "hono";
+import { env } from "hono/adapter";
+import { poweredBy } from "hono/powered-by";
 // @ts-ignore This file won’t exist if it hasn’t yet been built
-import * as build from "./build/server"; // eslint-disable-line import/no-unresolved
+import * as build from "./build/server";
 import { getLoadContext } from "./load-context";
 
 // biome-ignore lint/suspicious/noExplicitAny: This is a Cloudflare Worker handler
 const handleRemixRequest = createRequestHandler(build as any as ServerBuild);
 
-export default {
-  async fetch(request, env, ctx) {
-    try {
-      const loadContext = getLoadContext({
-        request,
-        context: {
-          cloudflare: {
-            // This object matches the return value from Wrangler's
-            // `getPlatformProxy` used during development via Remix's
-            // `cloudflareDevProxyVitePlugin`:
-            // https://developers.cloudflare.com/workers/wrangler/api/#getplatformproxy
-            cf: request.cf,
-            ctx: {
-              waitUntil: ctx.waitUntil.bind(ctx),
-              passThroughOnException: ctx.passThroughOnException.bind(ctx),
-            },
-            caches,
-            env,
+const app = new Hono();
+
+app.use(poweredBy());
+
+app.use("*", async (c) => {
+  const request = c.req.raw;
+  const ctx = c.executionCtx;
+
+  try {
+    const loadContext = getLoadContext({
+      request,
+      context: {
+        cloudflare: {
+          // This object matches the return value from Wrangler's
+          // `getPlatformProxy` used during development via Remix's
+          // `cloudflareDevProxyVitePlugin`:
+          // https://developers.cloudflare.com/workers/wrangler/api/#getplatformproxy
+          cf: request.cf,
+          ctx: {
+            waitUntil: ctx.waitUntil.bind(ctx),
+            passThroughOnException: ctx.passThroughOnException.bind(ctx),
           },
+          caches,
+          env: env(c),
         },
-      });
-      return await handleRemixRequest(request, loadContext);
-    } catch (error) {
-      console.log(error);
-      return new Response("An unexpected error occurred", { status: 500 });
-    }
-  },
-} satisfies ExportedHandler<Env>;
+      },
+    });
+    return await handleRemixRequest(request, loadContext);
+  } catch (error) {
+    console.log(error);
+    return new Response("An unexpected error occurred", { status: 500 });
+  }
+});
+
+export default app;
